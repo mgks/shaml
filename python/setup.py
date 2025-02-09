f2fe3aa1f6d27a673761c9d6c5a737b7cc29ae47
mgks <hello@mgks.dev>
2025-02-09 12:26:24 +0530

    python package update

--- python/setup.py
+++ python/setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='shaml',
-    version='0.0.1',
+    version='0.0.2',
     packages=find_packages(),
     install_requires=[],
     author='Ghazi Khan',
